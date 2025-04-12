d87c079117d604dacf230e8c05add7c6dacb6823
yacine <yacine.farhi@universite-paris-saclay.fr>
2025-04-12 17:32:12 -0400

    test

--- face_app.py
+++ face_app.py
@@ -1,6 +1,16 @@
 import cv2 
 import face_recognition
 import numpy as np 
+import os
+
+label = input("Enter the label (ex: yacine): ")
+
+# Create main path to save 
+base_dir = "faces_dataset"
+save_dir = os.path.join(base_dir, label)
+
+# Create the folder if not existing
+os.makedirs(save_dir, exist_ok=True)
 
 cam = cv2.VideoCapture(0)
 cv2.namedWindow("Face Recognition App")
@@ -9,7 +19,7 @@ img_counter = 0
 while True: 
     ret,frame =  cam.read()     
     if not ret: 
-        print("fail to grab frame")
+        print("Failed to grab frame")
         break
     cv2.imshow("test",frame)
 
@@ -21,7 +31,8 @@ while True:
         break
     #space key
     elif k%256 ==32: 
-        img_name = "faces_dataset/yacine/opencv_frame_{}.png".format(img_counter)
+        img_name = f"{label}_{img_counter}.png"
+        img_path = os.path.join(save_dir, img_name)
         cv2.imwrite(img_name,frame)
         print("Screenshot taken")
         img_counter+=1 
